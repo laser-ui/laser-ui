9aaa9846386f3abfc253d61dc13798a4e28a607b
xiejay97 <xiejay97@gmail.com>
2023-09-22 17:09:11 +0800

    chore: add executor for css

--- tools/scripts/build-themes.ts
+++
@@ -1,5 +0,0 @@
-import { copyFileSync, copySync } from 'fs-extra';
-import { join } from 'path';
-
-copySync(join(__dirname, '../../libs/themes'), join(__dirname, '../../dist/libs/themes'));
-copyFileSync(join(__dirname, '../../LICENSE'), join(__dirname, '../../dist/libs/themes/LICENSE'));
