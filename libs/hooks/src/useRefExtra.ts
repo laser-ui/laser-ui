18c6aaffadf1f1e06dbb5c0a0a8642a7df8c2ed2
xiejay <xiejay97@gmail.com>
2024-12-17 20:20:21 +0800

    fix(hooks): fix cant get ref after render by `useRefExtra`

--- libs/hooks/src/useRefExtra.ts
+++ libs/hooks/src/useRefExtra.ts
@@ -1,21 +1,41 @@
-import { isString, isUndefined } from 'lodash';
-import { useRef } from 'react';
+import { isNull, isString, isUndefined } from 'lodash';
+import { useMemo } from 'react';
 
 import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 
 export type RefExtra<T = HTMLElement> = (() => T | null) | string;
 
+class Ref {
+  private _current: any = null;
+
+  public refExtra?: RefExtra<any>;
+
+  public get current(): string {
+    if (isNull(this._current)) {
+      this.getCurrent();
+    }
+    return this._current;
+  }
+
+  public getCurrent() {
+    if (!isUndefined(this.refExtra)) {
+      this._current = isString(this.refExtra) ? document.querySelector(this.refExtra) : this.refExtra();
+    } else {
+      this._current = null;
+    }
+  }
+}
+
 export function useRefExtra(refExtra?: string): React.RefObject<HTMLElement | null>;
 export function useRefExtra<T = HTMLElement>(refExtra?: () => T | null): React.RefObject<T | null>;
 export function useRefExtra<T = HTMLElement>(refExtra?: RefExtra<T>): React.RefObject<T | null>;
 export function useRefExtra(refExtra?: RefExtra<any>): React.RefObject<any> {
-  const ref = useRef<any>(null);
+  const ref = useMemo(() => new Ref(), []);
+  ref.refExtra = refExtra;
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useIsomorphicLayoutEffect(() => {
-    if (!isUndefined(refExtra)) {
-      ref.current = isString(refExtra) ? document.querySelector(refExtra) : refExtra();
-    }
+    ref.getCurrent();
   });
 
   return ref;
